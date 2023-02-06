b7ef5f7f20365b0a6c1bb0c14bbe625f2fe4bd29
AywinK <115905373+AywinK@users.noreply.github.com>
2023-02-06 11:26:19 +0000

    completes contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,8 +17,21 @@ function Contact() {
                     <ContactForm />
                 </div>
 
-                <div className="col-md-6">
-                    <button type="button" className="cvBtn" onClick={e => {
+                <div className="col-md-6 d-flex flex-column align-items-center justify-content-around text-center fs-3 my-5 contactInfo">
+                    <h2 className="fs-1" style={{color: "var(--customDarkBlue)"}}>Let's Connect!</h2>
+                    <div className="my-3">
+                    <i class="fa-brands fa-square-github mx-3"></i>
+                    <a href="https://github.com/AywinK" className="anchor">On GitHub</a>
+                    </div>
+                    <div className="my-3">
+                        <i class="fa-brands fa-linkedin mx-3"></i>
+                        <a href="https://www.linkedin.com/in/aywin-kattappuram-7a3445132/" className="anchor">On LinkedIn</a>
+                    </div>
+                    <div className="mt-3">
+                        <i class="fa-solid fa-envelope mx-3"></i>
+                        <a href="mailto:aywingeorge@hotmail.co.uk" className="anchor">aywingeorge@hotmail.co.uk</a>
+                    </div>
+                    <button type="button" className="cvBtn my-5" onClick={e => {
                         e.preventDefault();
                         window.open("https://drive.google.com/file/d/1skRkBP5KMXVQKNCtNRd5HQEa8UGZE2l8/view?usp=sharing")
                     }}>View my CV</button>
