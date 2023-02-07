97106a03c9e7ea24a010db136ec71e896b41cfef
AywinK <115905373+AywinK@users.noreply.github.com>
2023-02-07 16:15:16 +0000

    wip feature/projects

---
+++ src/components/Project.js
@@ -0,0 +1,27 @@
+
+
+
+function Project(projectProp) {
+
+    // const projectImg = projectProp.image;
+    
+    const liveLink = projectProp.live;
+    // const placeholderKey = Math.floor(Math.random() * 100000000);
+
+    return (
+        <div className="p-2 my-5">
+            <img src={projectProp.image} className="img-responsive shadow-4-strong py-2" alt={projectProp.title}></img>
+            <h3>{projectProp.title}</h3>
+            <button type="button" className="cvBtn my-2" onClick={e => {
+                e.preventDefault();
+                const repoLink = projectProp.repo;
+                window.open(repoLink)
+            }}>View GitHub Repo</button>
+            <div className="mt-3">
+                <a href={liveLink} className="anchor">Visit Deployed Page</a>
+            </div>
+        </div>
+    );
+}
+
+export default Project
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,27 @@
 
+import Project from "./Project";
+import ProjectData from "./ProjectData.json";
+
 
 function Projects() {
+
+    const customBackground = {
+        backgroundImage: "radial-gradient(circle at 360%, var(--customPurple), var(--customBlue) 60%, var(--customBlue) 75%, var(--customPurple) 95%)",
+    };
+
     return (
-        <div className="container-fluid bg-dark">
-            <h2>Projects</h2>
+        <div className="text-white container-fluid grid text-center" style={customBackground}>
+            <h2 className="pt-3 customText fs-1 row justify-content-center">Projects</h2>
+            <main className="row justify-content-center align-items-center">
+                {ProjectData.data.map((projectObj, index) => {
+                    // console.log(index);
+                    return (
+                        <div className="col-lg-4 col-md-6 col-12 projectItem g-3" key={index}>
+                        {Project(projectObj)}
+                        </div>
+                    )
+                    })}
+            </main>
         </div>
     );
 }
