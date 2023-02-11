e9dd8707f599df6df831e12d1d3f4764fe5e6560
AywinK <115905373+AywinK@users.noreply.github.com>
2023-02-11 09:34:28 +0000

    adds project page component and routes

--- src/components/ProjectThumbnail.js
+++ src/components/ProjectThumbnail.js
@@ -1,5 +1,5 @@
 
-import { Route, Routes, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function ProjectThumbnail(props) {
 
@@ -10,7 +10,7 @@ function ProjectThumbnail(props) {
         <div className="projectItem p-2">
             {/* <img src={require(`../assets/images/${projectProp.image}`)}  className="shadow-4-strong pt-1 pb-3 projectImg img-fluid" alt={projectProp.title}></img> */}
             <NavLink to={`/Projects/${props.projectObj.title}`} className="navlinkComponent">
-            <img src={require(`../assets/images/webpageThumbnail.png`)}  className="shadow-4-strong pt-1 pb-3 projectImg img-fluid" alt={props.projectObj.title}></img>
+            <img src={require(`../assets/images/webpageThumbnail.png`)}  className="shadow-4-strong pt-1 pb-3 img-fluid" alt={props.projectObj.title}></img>
             <h2>{props.projectObj.title}</h2>
             </NavLink>
             {/* <button type="button" className="cvBtn my-2" onClick={e => {
