6136b243a39066d4a68d4ba6b10f486b9045c0e4
AywinK <115905373+AywinK@users.noreply.github.com>
2023-03-14 20:29:38 +0000

    adds techstack icons to thumbnail and updates json

--- src/components/ProjectThumbnail.js
+++ src/components/ProjectThumbnail.js
@@ -8,15 +8,22 @@ function ProjectThumbnail(props) {
             top: 0,
             behavior: "smooth"
         })
+
     }
-    
+
     return (
         <div className="projectItem p-2">
             <NavLink to={`/Projects/${props.projectObj.title}`} className="navlinkComponent" onClick={scrollUp}>
                 <img src={require(`../assets/images/webpageThumbnail.png`)} className="shadow-4-strong pt-1 pb-3 img-fluid" alt={props.projectObj.title}></img>
                 <h2>{props.projectObj.title}</h2>
+                <div>
+                    {props.projectObj.techIconsArr.map(icon => {
+                        return (
+                            <img src={require(`../assets/icons/${icon}.png`)} alt={icon} key={icon} className="p-2" height="50px"></img>
+                        )
+                    })}
+                </div>
             </NavLink>
-
         </div>
     );
 }
